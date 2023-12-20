b60d615899db12445e066f2288fb5926a80315cf
Eduard Ros <maxshadow@hotmail.com>
2023-12-20 10:36:21 +0100

    modificado el if statement de comandos para que sea un switch, mas eficiente y menos codigo

--- src/classes/execs/server.cpp
+++ src/classes/execs/server.cpp
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 20:07:51 by rabril-h          #+#    #+#             */
-/*   Updated: 2023/12/17 19:41:01 by eros-gir         ###   ########.fr       */
+/*   Updated: 2023/12/20 10:34:55 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@ void  Server::_runCommand(std::vector<std::string> vec, int const clientFd)
   const std::string commands[10] = {"JOIN", "USER", "NICK", "PRIVMSG",
                         "INVITE", "TOPIC", "NAMES", "MODE", "KICK", "PING"};
 
-  
-
+  size_t icomm = 0;
 
   (void)clientFd;
   
-
-
 	// void	(Server::*f[10])(int const client_fd, cmd &info) = \
 	// {&Server::_join, &Server::_user, &Server::_nick, &Server::_privmsg,\
 	//  &Server::_invite, &Server::_topic, &Server::_names,\
@@ -46,74 +43,49 @@ void  Server::_runCommand(std::vector<std::string> vec, int const clientFd)
 
   // TODO make case for PASS here so we can check if this is Client trying to get to server for the first time
 
-  for (size_t i = 0; i < size; i++)
-  {    
-    
-    if (input == commands[i])
-    {
-        if (input == "JOIN")
-        {
-         Join join = Join(clientFd, vec, this);  
-
-        }
-        else if (input == "USER")
-        {
-          std::cout << "handling USER on client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "NICK")
-        {
-          std::cout << "handling NICK on client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "PRIVMSG")
-        {
-          Privmsg privmsg = Privmsg(clientFd, vec, this);
-          std::cout << "handling PRIVMSG on client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "INVITE")
-        {
-          std::cout << "handling INVITEon client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "TOPIC")
-        {
-          std::cout << "handling TOPIC on client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "NAMES")
-        {
-          std::cout << "handling NAMES on client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "MODE")
-        {
-          std::cout << "handling MODE on client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "KICK")
-        {
-          std::cout << "handling KICK on client [" << clientFd << "] with params :" << std::endl;
-
-        }
-        else if (input == "PING")
-        {
-
-          std::cout << "handling PING on client [" << clientFd << "] with params :" << std::endl;
-        }
-        else
-        {
-          std::cout << input << " is an unhandled command" << std::endl;          
-        }
-
-        //this->_printVector(vec, "");
-    }
-
+  while (icomm < size)
+  {
+    if (input == commands[icomm])
+        break;
+    icomm++;
   }
-
-
-  
+  switch (icomm)
+  {
+    case 0: { // JOIN
+      Join join = Join(clientFd, vec, this);  
+    }  break;
+    case 1: { // USER
+        std::cout << "handling USER on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 2: { // NICK
+        std::cout << "handling NICK on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 3: { // PRIVMSG
+        Privmsg privmsg = Privmsg(clientFd, vec, this);
+        std::cout << "handling PRIVMSG on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 4: { // INVITE
+        std::cout << "handling INVITE on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 5: { // TOPIC
+        std::cout << "handling TOPIC on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 6: { // NAMES
+        std::cout << "handling NAMES on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 7: { // MODE
+        std::cout << "handling MODE on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 8: { // KICK
+        std::cout << "handling KICK on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    case 9: { // PING
+        std::cout << "handling PING on client [" << clientFd << "] with params :" << std::endl;
+    } break;
+    default: {
+        std::cout << input << " is an unhandled command" << std::endl;
+    } break;
+  } 
 
 
 	// if (checkInit(_clients[client_fd], c))
