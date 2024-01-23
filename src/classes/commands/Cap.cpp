d2d4b1e8ca43349661fa9a19740ca96d983b0cb9
Eduard Ros <maxshadow@hotmail.com>
2024-01-23 10:44:45 +0100

    corregido un error del welcome message

--- src/classes/commands/Cap.cpp
+++ src/classes/commands/Cap.cpp
@@ -6,7 +6,7 @@
 /*   By: eros-gir <eros-gir@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 09:49:58 by eros-gir          #+#    #+#             */
-/*   Updated: 2024/01/23 10:41:15 by eros-gir         ###   ########.fr       */
+/*   Updated: 2024/01/23 10:44:16 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ Cap::Cap(int const &clientFd, std::vector<std::string> const &vec, Server *serve
 	  client->sendMessage("CAP * END");
 	  client->sendMessage(Messages::getUserWelcome(client->getName(), "127.0.0.1", client->getNickName(), client->getHostName()));
 	  client->sendMessage(Messages::getYourHost(client->getNickName(), "127.0.0.1"));
-	  client->sendMessage(Messages::getCreatedAt(client->getNickName(), server->getCurrentTime()));
+	  client->sendMessage(Messages::getCreatedAt(client->getNickName(), server->getServerCreationTime()));
 	  client->sendMessage(Messages::getMyInfo(client->getNickName(), "127.0.0.1"));
 	  break;
 	}
